8b4c54ad6524fb7e40c67a097c37a2eaf3bc1f9c
tri2k5123 <96030017+tri2k5123@users.noreply.github.com>
2024-05-26 16:07:46 +0700

    twelveth commit

--- ryanflo/app/admin/users/page.js
+++ ryanflo/app/admin/users/page.js
@@ -1,10 +1,10 @@
-
+"use client"
 import SectionAdmin from "@/components/layouts/SectionAdmin";
 import AdUsers from "@/components/layouts/admin/AdUsers";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
-export default async function Categories() {
+export default function Categories() {
     const route = useRouter();
     const { data: session } = useSession();
     if(session?.user?.role !== "admin") route.push("/")
