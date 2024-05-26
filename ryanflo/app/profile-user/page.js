918a1ba6d834c374c7458d5d4049402a5c7b02f3
tri2k5123 <96030017+tri2k5123@users.noreply.github.com>
2024-05-26 11:36:57 +0700

    seventh commit

--- ryanflo/app/profile-user/page.js
+++ ryanflo/app/profile-user/page.js
@@ -1,31 +1,9 @@
-import Profile from '@/components/layouts/Profile'
-import { getServerSession } from 'next-auth'
-import { authOptions } from '../api/auth/[...nextauth]/route'
-import { redirect } from 'next/navigation';
+"use client";
 
-const getProfile = async (email) => {
-    try {
-        const resGetProfile = await fetch(`/api/profile?email=${email}`, {
-            method: "GET"
-        })
-        return resGetProfile.json();
-    } catch (error) {
-        
-    }
-}
+import ServerPageProfileUser from "@/components/layouts/ServerpageProfileUser";
 
-
-export default async function ProfileUser() {
-    const session = await getServerSession(authOptions);
-    if(!session) redirect("/");
-    const { matchUserInfo } = await getProfile(session?.user?.email);
-   
+export default function ProfileUser() {
     return (
-        <div className="mt-[72px] mx-12 mb-6">
-            <div className='gridIn wide'>
-                <Profile matchUserInfo={matchUserInfo}/>
-            </div>
-
-        </div>
+        <ServerPageProfileUser/>
     )
 }
