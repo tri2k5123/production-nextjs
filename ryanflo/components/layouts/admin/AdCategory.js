7e5b1f468035955b2f832f96839e9694cd7ed2a3
tri2k5123 <96030017+tri2k5123@users.noreply.github.com>
2024-05-26 16:21:14 +0700

    thirdth commit

--- ryanflo/components/layouts/admin/AdCategory.js
+++ ryanflo/components/layouts/admin/AdCategory.js
@@ -1,25 +1,32 @@
+"use client";
+import { useEffect, useState } from "react";
 import ListCategories from "./ListCategory";
 import SectionCategory from "./SectionCategory";
 
-const getCategories = async () => {
-    try {
-        const resGetCategory = await fetch("/api/category", {
-        method: "GET"
-    })
-    return resGetCategory.json();
-    } catch (error) {
-        
-    }
-}
 
-export default async function AdCategory() {
-    
-    const { listCategories } =  await getCategories();
+
+export default function AdCategory() {
+    const [ listCategories, setListCategories ] = useState();
  
+    useEffect(() => {
+        getCategories()
+    }, [])
+    const getCategories = async () => {
+        try {
+            const resGetCategory = await fetch("/api/category", {
+            method: "GET"
+        })
+        const { listCategories } =  await resGetCategory.json();
+        setListCategories(listCategories)
+        } catch (error) {
+            
+        }
+    }
+
     return (
         <>
             <SectionCategory />
-            <ListCategories categories={listCategories}/>
+            {listCategories && <ListCategories categories={listCategories}/>}
         </>
     )
 }
--- ryanflo/middleware.js
+++ ryanflo/middleware.js
@@ -1,6 +1,4 @@
 export { default } from "next-auth/middleware";
 export const config = { matcher: [
     "/profile-user",
-    "/admin",
-
 ] }
